20540ed66d7f2233b4fe0b9347097ce8dc03752f
James Shore <jshore@jamesshore.com>
2014-03-03 15:38:27 -0800

    Set up JSHint in automated build

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,18 @@
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 "use strict";
 
+var jshint = require("simplebuild-jshint");
+
 task("default", ["lint"]);
 
 desc("Lint the code");
 task("lint", function() {
-	console.log("Lint code goes here");
-});
+	jshint.checkFiles({
+		files: "*.js",
+		options: {},
+		globals: {}
+	}, complete, fail);
+}, { async: true });
 
 desc("Run a local server for manual testing");
 task("run", function() {
