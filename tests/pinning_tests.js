2a67993e5941085f72ddea6f8c5f21cdf46d46ed
James Shore <jshore@jamesshore.com>
2014-03-04 11:38:16 -0800

    Started on implementing approval test assertions

--- tests/pinning_tests.js
+++ tests/pinning_tests.js
@@ -4,6 +4,8 @@
 (function() {
 	"use strict";
 
+	var fs = require("fs");     // NOTE! This is the PhantomJS fs module, not Node's fs module
+
 	var URL = "http://localhost:5000/example/multi-page/index.html";
 
 	casper.test.setUp(function(done) {
@@ -13,28 +15,61 @@
 	casper.test.begin("Navigate through all steps using the mouse", function(test) {
 		test.assertExists("#startButton");
 
-
+		checkDom("start.txt", test);
 		casper.thenClick("#startButton");
-		thenClickNext();        // step 2
-		thenClickNext();        // step 3
-		thenClickNextPage();    // step 4
-		thenClickNext();        // step 5
-		thenClickNextPage();    // done
+		checkDom("step1.txt", test);
+//		thenClickNext();        // step 2
+//		checkDom("step2.txt");
+//		thenClickNext();        // step 3
+//		checkDom("step3.txt");
+//		thenClickNextPage();    // step 4
+//		checkDom("step4.txt");
+//		thenClickNext();        // step 5
+//		checkDom("step5.txt");
+//		thenClickNextPage();    // done
+//		checkDom("end.txt");
+
 		casper.run(function() {
-				casper.capture("deleteme.png");
 				test.done();
 		});
 	});
 
 	function thenClickNext() {
-		return casper.thenClick(".introjs-nextbutton")
-			.wait(1000);
+		return thenClickSelector(".introjs-nextbutton");
 	}
 
 	function thenClickNextPage() {
-		return casper.thenClick(".introjs-skipbutton")
-			.wait(1000);
+		return thenClickSelector(".introjs-skipbutton");
+	}
 
+	function thenClickSelector(selector) {
+		return casper.thenClick(selector)
+			.wait(1000);
+	}
+
+	function checkDom(filename, test) {
+		casper.then(function() {
+			var actualDom = this.evaluate(function() {
+				return document;
+			});
+			actualDom = JSON.stringify(actualDom);
+
+			var expectedDom = fs.read("./tests/approvals/" + filename);
+
+
+			test.assertEquals(actualDom, expectedDom, filename);
+		});
+	}
+
+
+	function dumpDom(filename) {
+		casper.then(function() {
+			var dom = this.evaluate(function() {
+				return document;
+			});
+			dom = JSON.stringify(dom);
+			fs.write("./tests/approvals/" + filename, dom, "w");
+		});
 	}
 
 }());
