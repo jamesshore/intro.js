60b36fc502ddfb8acc9b449fcd1501e9627a581b
James Shore <jshore@jamesshore.com>
2014-06-04 15:23:37 -0700

    Added test for scrolling down

--- src/_scroll_test.js
+++ src/_scroll_test.js
@@ -5,26 +5,40 @@ var scroll = require("./scroll.js");
 
 describe("scrollToTargetElement", function() {
 
-	var div;
+	var target;
+	var spacer;
 
 	beforeEach(function() {
-		div = document.createElement("div");
-		div.style = "position: absolute; top: 0px; height: 100px; border: solid red 1px;";
-		div.innerHTML = "hi";
-		document.body.appendChild(div);
+		target = document.createElement("div");
+		target.style = "position: absolute; top: 0px; height: 100px; border: solid red 1px;";
+		target.innerHTML = "target";
+		document.body.appendChild(target);
+
+		spacer = document.createElement("div");
+		spacer.style = "position: absolute; top: 11000px; height: 200px; border: solid red 1px;";
+		spacer.innerHTML = "spacer";
+		document.body.appendChild(spacer);
 	});
 
 	afterEach(function() {
-		document.body.removeChild(div);
+		document.body.removeChild(target);
+		document.body.removeChild(spacer);
 	});
 
 	it("scrolls down to put element in viewport", function() {
-		div.style.top = "10000px";
-		div.style.paddingBottom = "200px";
+		target.style.top = "10000px";
 
-		scroll.toDomElement(null, div);
-
-		expect(window.pageYOffset).to.equal(10000 - 30);    // 30 pixels of padding
+		scroll.toDomElement(null, target);
+		expect(window.pageYOffset).to.equal(9956);
 	});
+
+	it("scrolls up to put element in viewport", function() {
+		target.style.top = "1000px";
+
+		window.scroll(0, 10000);
+		scroll.toDomElement(null, target);
+		expect(window.pageYOffset).to.equal(970);
+	});
+
 
 });
