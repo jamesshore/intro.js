9efb99f6ea31068edf08c88d7bb8530ed01e4a52
James Shore <jshore@jamesshore.com>
2014-06-04 13:44:12 -0700

    Factored scrollToTargetElement() into scroll.js

--- src/intro.js
+++ src/intro.js
@@ -6,6 +6,8 @@
 * Copyright (C) 2013 usabli.ca - A weekend project by Afshin Mehrabani (@afshinmeh)
 */
 
+var scroll = require("./scroll.js");
+
 //Default config/variables
 var VERSION = '0.7.0';
 
@@ -427,7 +429,7 @@ function _showElement(targetElement) {
   setButtonClassesAndSkipButtonLabel();
   setFocusToNextButton();
   positionTargetElementAboveTooltip();
-  if (self._options.scrollToElement === true) scrollToTargetElement(self, targetElement);
+  if (self._options.scrollToElement === true) scroll.toTargetElement(self, targetElement);
 
   fireAfterChangeEvent();
 	return;
@@ -667,61 +669,6 @@ function _showElement(targetElement) {
   }
 
 }
-
-
-function scrollToTargetElement(self, targetElement) {
-  if (!_elementInViewport(targetElement.element)) {
-	  var rect = targetElement.element.getBoundingClientRect(),
-		  winHeight = _getWinSize().height,
-		  top = rect.bottom - (rect.bottom - rect.top),
-		  bottom = rect.bottom - winHeight;
-	  //Scroll up
-	  if (top < 0 || targetElement.element.clientHeight > winHeight) {
-		  window.scrollBy(0, top - 30); // 30px padding from edge to look nice
-		  //Scroll down
-	  }
-	  else {
-		  window.scrollBy(0, bottom + 100); // 70px + 30px padding from edge to look nice
-	  }
-  }
-}
-
-/**
-* Check if element is visible
-* http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-*
-* @api private
-* @method _elementInViewport
-* @param {Object} el
-*/
-function _elementInViewport(el) {
- var rect = el.getBoundingClientRect();
-
- return (
-   rect.top >= 0 &&
-   rect.left >= 0 &&
-   (rect.bottom+80) <= window.innerHeight && // add 80 to get the text right
-   rect.right <= window.innerWidth
- );
-}
-
-/**
-* Provides a cross-browser way to get the screen dimensions
-* via: http://stackoverflow.com/questions/5864467/internet-explorer-innerheight
-*
-* @api private
-* @method _getWinSize
-* @returns {Object} width and height attributes
-*/
-function _getWinSize() {
- if (window.innerWidth != undefined) {
-   return { width: window.innerWidth, height: window.innerHeight };
- } else {
-   var D = document.documentElement;
-   return { width: D.clientWidth, height: D.clientHeight };
- }
-}
-
 
 
 /**
